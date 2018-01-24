1838154e692433a5febecab1b581d195ec6b0029
Kim Schulze <kimberlykayschulze@gmail.com>
2018-01-24 14:30:56 -0800

    Add project files.

---
+++ BrauerNet2.1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BrauerNet2._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
